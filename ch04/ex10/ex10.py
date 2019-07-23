0aaf8755b6d6f2e26819eaf938569a7dfa0f96c6
aruga <ryoh.aruga@nts.ricoh.co.jp>
2019-07-23 19:44:28 +0900

    [add]Add ex10

--- ch04/ex10/ex10.py
+++ ch04/ex10/ex10.py
@@ -1,2 +1,16 @@
-#@title ch04-10
+# @title ch04-10
+def decorator(func):
+    def decorated_func(*args, **kwargs):
+        print('start')
+        result = func(*args, **kwargs)
+        print('end')
+        return result
+    return decorated_func
 
+
+@decorator
+def test_func():
+    print('test_func')
+
+
+test_func()
