d70a4be5bc7ffced7d057d5db503507d67a9fde0
aruga <ryoh.aruga@nts.ricoh.co.jp>
2019-07-23 19:43:44 +0900

    [add]Add ex09

--- ch04/ex09/ex09.py
+++ ch04/ex09/ex09.py
@@ -1,3 +1,9 @@
-#@title ch04-09
+# @title ch04-09
+def get_odds(first, last, step):
+    for number in range(first, last, step):
+        yield number
 
-print('done')
+
+for i, odd in enumerate(get_odds(1, 10, 2), 1):
+    if i == 3:
+        print(odd)
