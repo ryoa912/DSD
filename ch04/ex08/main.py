3539bb5d5e404a9f0b6e2ec29fe88f68c60f07ae
Jun Kimura <jun.kimura@nts.ricoh.co.jp>
2019-07-24 13:04:02 +0900

    回答作った。

---
+++ ch04/ex08/main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""4-8
+['Harry', 'Ron', 'Hermione']というリストを返すgoodという関数を定義しよう。
+"""
+
+def good():
+    return ['Harry', 'Ron', 'Hermione']
+
+def chapter4_exam8():
+    print(good())
+
+if __name__ == "__main__":
+    chapter4_exam8()
