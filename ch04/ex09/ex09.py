4c1e66ac537cd0d1cadb06f5898cee48613c7c14
aruga <ryoh.aruga@nts.ricoh.co.jp>
2019-07-24 15:06:01 +0900

    [fix]Fix ex09-12 as function name differ to action

--- ch04/ex09/ex09.py
+++ ch04/ex09/ex09.py
@@ -1,10 +1,10 @@
 # @title ch04-09
-def get_odds(first, last, step):
-    for number in range(first, last, step):
+def get_odds():
+    for number in range(1, 10, 2):
         yield number
 
 
-for i, odd in enumerate(get_odds(1, 10, 2), 1):
+for i, odd in enumerate(get_odds(), 1):
     if i == 3:
         print(odd)
 
