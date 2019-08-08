a229cf36ddb0bcc0a19860810ebf76ca3bde70d5
Naoyuki Shimizu <sausupo0619@yahoo.co.jp>
2019-08-08 18:12:33 +0900

    誤字修正

--- ch05/ex07/main.py
+++ ch05/ex07/main.py
@@ -2,8 +2,8 @@ from collections import defaultdict
 
 dict_of_lists = defaultdict(list)
 
-dict_of_lists['a'].append('somethis for a')
+dict_of_lists['a'].append('something for a')
 
 print(dict_of_lists['a'])
 
-# ['somethis for a']
+# ['something for a']
