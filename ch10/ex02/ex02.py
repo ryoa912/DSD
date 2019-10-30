5e43938b64780b1d9b178688172c67d73bb15a04
ryoh.aruga <ryoh.aruga@nts.ricoh.co.jp>
2019-10-30 10:10:36 +0900

    [add]ex10-02

---
+++ ch10/ex02/ex02.py
@@ -0,0 +1,13 @@
+# @title ch10-ex02
+# テキストファイルtoday.txtの内容をtoday_stringという文字列変数に読み込もう。
+import os.path
+fin = open(os.path.dirname(__file__) + '/' + 'today.txt', 'rt')
+today_string = fin.read()
+fin.close()
+print('today_string: ' + today_string)
+
+# result:
+# today_string: 2019-10-30
+
+# memo:
+# 巨大なファイルをチャンクを使わずにread()したらどうなるのか
